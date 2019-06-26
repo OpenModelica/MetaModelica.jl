2f30bcea806a86f377a0e6c0ca952dc65b0befb8
johti <johti17@liu.se>
2019-06-26 20:39:30 +0200

    Now includes correct stuff

--- src/MetaModelica.jl
+++ src/MetaModelica.jl
@@ -4,6 +4,7 @@ include("list.jl")
 include("matchcontinue.jl")
 include("metaModelicaTypes.jl")
 include("union.jl")
+include("functionInheritance.jl")
 
 export @match, @matchcontinue, MatchFailure, ModelicaReal, ModelicaInteger
 export @Uniontype, @Record, @UniontypeDecl, @ExtendFunction
