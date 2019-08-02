f1965a79f8604221601f4b02259c44264915dd3f
John <JKRT@users.noreply.github.com>
2019-08-02 14:58:59 +0200

    Update dangerous.jl

--- src/dangerous.jl
+++ src/dangerous.jl
@@ -8,52 +8,51 @@ module Dangerous
 import ExportAll
 using ..MetaModelica
 
-#= O(1) =#
+""" O(1) """
 function arrayGetNoBoundsChecking(arr::Array{A}, index::ModelicaInteger)::A where {A <: Any}
   @inbounds arr[index]
 end
 
-#= O(1) =#
+""" O(1) """
 function arrayUpdateNoBoundsChecking(arr::Array{A}, index::ModelicaInteger, newValue::A)::Array{A} where {A <: Any}
-  local newArray::Array{A}
-  newArray = arr
+  local newArray::Array{A} = arr
   @inbounds newArray[index] = newValue
   newArray
 end
 
-#= Creates a new array where the elements are *not* initialized!. Any attempt to
+""" Creates a new array where the elements are *not* initialized!. Any attempt to
 access an uninitialized elements may cause segmentation faults if you're
 lucky, and pretty much anything else if you're not. Do not use unless you will
 immediately fill the whole array with data. The dummy variable is used to fix
-the type of the array. =#
+the type of the array. """
 function arrayCreateNoInit(size::ModelicaInteger, dummy::A)::Array{A} where {A <: Any}
   local arr::Array{A}  
   arr = fill(arr, 0)
 end
 
-#= O(1) =#
+""" O(1) """
 function stringGetNoBoundsChecking(str::String, index::ModelicaInteger)::ModelicaInteger
   local ch::ModelicaInteger
   ch = @inbounds str[index]
 end
 
-#= Not possible unless we write a C list impl for Julia =#
+""" Not possible unless we write a C list impl for Julia """
 function listReverseInPlace(inList::List{T})::List{T} where {T <: Any}
   MetaModelica.listReverse(inList)
 end
 
-#= O(1). A destructive operation changing the \\\"first\\\" part of a cons-cell. =#
+""" O(1). A destructive operation changing the \\\"first\\\" part of a cons-cell. """
 function listSetFirst(inConsCell #= A non-empty list =#::List{A}, inNewContent::A) where {A <: Any}
   #= Defined in the runtime =#
 end
 
-#= O(1). A destructive operation changing the \\\"rest\\\" part of a cons-cell.
+""" O(1). A destructive operation changing the \\\"rest\\\" part of a cons-cell """
 NOTE: Make sure you do NOT create cycles as infinite lists are not handled well in the compiler. =#
 function listSetRest(inConsCell #= A non-empty list =#::List{A}, inNewRest::List{A}) where {A <: Any}
   #= Defined in the runtime =#
 end
 
-#= O(n) =#
+""" O(n) """
 function listArrayLiteral(lst::List{A})::Array{A} where {A <: Any}
   local arr::Array{A}
 
